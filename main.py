78248e5576bc03d431b5ea992837521b944e2a97
どら <dora@natamoko.jp>
2026-02-12 22:16:47 +0900

    change main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from config import DISCORD_BOT_TOKEN
 # 起動設定
 
 intents = discord.Intents.default()
-intents.members = True
 intents.message_content = True
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
@@ -16,6 +15,11 @@ async def on_ready():
   await tree.sync()
   await client.change_presence(status=discord.Status.online, activity=discord.Game(name="Hello World!"))
   print(discord.__version__)
+
+@client.event
+async def on_message(message):
+  if not message.author.bot:
+    print(message.content)
 
 # 起動
 
