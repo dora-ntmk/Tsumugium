aea8c5d796b89ab842213647e54ab7065e1738dc
どら <dora@natamoko.jp>
2026-02-14 14:19:37 +0900

    main.py更新　ボイスチャンネル接続・切断処理の追加（簡易）

--- main.py
+++ main.py
@@ -18,8 +18,33 @@ async def on_ready():
   await client.change_presence(status=discord.Status.online, activity=discord.Game(name=stts))
   print(discord.__version__)
 
+# 入室
+@tree.command(
+  name="join",
+  description="ボイスチャンネルに接続します。"
+)
+async def join(ctx):
+  await ctx.response.defer()
+  if ctx.user.voice:
+    await ctx.user.voice.channel.connect(timeout=60, self_deaf=True)
+    await ctx.edit_original_response(content="ボイスチャンネルに接続しました")
+  else:
+    await ctx.edit_original_response(content="ボイスチャンネルに接続できませんでした")
 
-# 音声生成
+# 退室
+@tree.command(
+  name="leave",
+  description="ボイスチャンネルから切断します。"
+)
+async def leave(ctx):
+  await ctx.response.defer()
+  if ctx.user.voice:
+    await ctx.guild.voice_client.disconnect()
+    await ctx.edit_original_response(content="ボイスチャンネルから切断しました")
+  else:
+    await ctx.edit_original_response(content="ボイスチャンネルから切断できませんでした")
+
+# 音声生成・再生
 @client.event
 async def on_message(message):
   if not message.author.bot:
